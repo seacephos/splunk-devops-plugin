92a518349b82c196c77185373e4edc38ac9a7f71
rajanthan <RekaThirunavukkarasu_Ajanthan@intuit.com>
2021-02-23 23:57:22 -0800

    merging maps into one

--- splunk-devops/src/main/java/com/splunk/splunkjenkins/listeners/LoggingQueueListener.java
+++ splunk-devops/src/main/java/com/splunk/splunkjenkins/listeners/LoggingQueueListener.java
@@ -6,14 +6,11 @@ import com.splunk.splunkjenkins.Constants;
 import com.splunk.splunkjenkins.SplunkJenkinsInstallation;
 import com.splunk.splunkjenkins.utils.SplunkLogService;
 import hudson.Extension;
-import hudson.model.Item;
 import hudson.model.Queue;
 import hudson.model.queue.QueueListener;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.logging.Logger;
 
 import static com.splunk.splunkjenkins.model.EventType.QUEUE_INFO;
 import static com.splunk.splunkjenkins.utils.LogEventHelper.getMasterStats;
@@ -37,52 +34,50 @@ public class LoggingQueueListener extends QueueListener {
     // Cache to keep track of queue time
     private final static Cache<Long, Float> cache = CacheBuilder.newBuilder()
             .maximumSize(3000).build();
-    //To keep track of blocked events timestamp
-    private final static ConcurrentHashMap<Long, Long> blockedQueue = new ConcurrentHashMap<Long, Long>();
-    //To keep track of buildable events timestamp
-    private final static ConcurrentHashMap<Long, Long> buildableQueue = new ConcurrentHashMap<Long, Long>();
-    //To keep track of waiting events timestamp
-    private final static ConcurrentHashMap<Long, Long> waitingQueue = new ConcurrentHashMap<Long, Long>();
+    //To keep track of events timestamp for the queue phases like waiting, blocked and buildable
+    private final static ConcurrentHashMap<Long, Long> queuePhase = new ConcurrentHashMap<Long, Long>();
 
     @Override
     public void onEnterWaiting(Queue.WaitingItem wi) {
-        sendToSplunkOnEnter(wi.task, wi.getId(), waitingQueue, Constants.ENQUEUE_TAG_NAME);
+        sendToSplunkOnEnter(wi.task, wi.getId(), Constants.ENQUEUE_TAG_NAME);
     }
 
     @Override
     public void onLeaveWaiting(Queue.WaitingItem wi) {
-        sendToSplunkOnLeft(wi.task, wi.getId(), waitingQueue,
+        sendToSplunkOnLeft(wi.task, wi.getId(),
                 Constants.WAITING_PHASE_NAME, wi.getWhy(), wi.getInQueueSince());
     }
 
     @Override
     public void onEnterBlocked(Queue.BlockedItem bi) {
-        sendToSplunkOnEnter(bi.task, bi.getId(), blockedQueue, Constants.BLOCKED_PHASE_NAME);
+        sendToSplunkOnEnter(bi.task, bi.getId(), Constants.BLOCKED_PHASE_NAME);
     }
 
     @Override
     public void onLeaveBlocked(Queue.BlockedItem bi) {
-        sendToSplunkOnLeft(bi.task, bi.getId(), blockedQueue,
+        sendToSplunkOnLeft(bi.task, bi.getId(),
                 Constants.BLOCKED_PHASE_NAME, bi.getWhy(), bi.getInQueueSince());
     }
 
     @Override
     public void onEnterBuildable(Queue.BuildableItem bi) {
-        sendToSplunkOnEnter(bi.task, bi.getId(), buildableQueue, Constants.BUILDABLE_PHASE_NAME);
+        sendToSplunkOnEnter(bi.task, bi.getId(), Constants.BUILDABLE_PHASE_NAME);
 
     }
 
     @Override
     public void onLeaveBuildable(Queue.BuildableItem bi) {
-        sendToSplunkOnLeft(bi.task, bi.getId(), buildableQueue,
+        sendToSplunkOnLeft(bi.task, bi.getId(),
                 Constants.BUILDABLE_PHASE_NAME, bi.getWhy(), bi.getInQueueSince());
     }
 
     @Override
     public void onLeft(Queue.LeftItem li) {
-        Float queueTime = sendToSplunkOnLeft(li.task, li.getId(), buildableQueue,
+        Float queueTime = sendToSplunkOnLeft(li.task, li.getId(),
                 Constants.DEQUEUE_TAG_NAME, li.getWhy(), li.getInQueueSince());
-        //Storing it in the cache to access it later
+        //Removing it from the map
+        queuePhase.remove(li.getId());
+        //Storing i]t in the cache to access it later
         cache.put(li.getId(), queueTime);
 
     }
@@ -131,12 +126,11 @@ public class LoggingQueueListener extends QueueListener {
      * Send build queue meta data to splunk on entering to any of the queue phases
      * like buildable, blocked and waiting
      *
-     * @param task task associated with a queue
-     * @param id id of the queue
-     * @param queue map used to store the time stamp of the queue phases
+     * @param task      task associated with a queue
+     * @param id        id of the queue
      * @param eventType type of the queue phase
      */
-    private void sendToSplunkOnEnter(Queue.Task task, Long id, ConcurrentHashMap queue, String eventType) {
+    private void sendToSplunkOnEnter(Queue.Task task, Long id, String eventType) {
         if (SplunkJenkinsInstallation.get().isEventDisabled(QUEUE_INFO)) {
             return;
         }
@@ -144,9 +138,9 @@ public class LoggingQueueListener extends QueueListener {
         if (SplunkJenkinsInstallation.get().isJobIgnored(name)) {
             return;
         }
-        queue.put(id, System.currentTimeMillis());
+        queuePhase.put(id, System.currentTimeMillis());
         String eventTypeEnqueue;
-        if(eventType.equals(Constants.ENQUEUE_TAG_NAME)) {
+        if (eventType.equals(Constants.ENQUEUE_TAG_NAME)) {
             eventTypeEnqueue = eventType;
         } else {
             eventTypeEnqueue = Constants.ENQUEUE_TAG_NAME + "_" + eventType;
@@ -160,15 +154,14 @@ public class LoggingQueueListener extends QueueListener {
      * Send queue time and other meta data to splunk on leaving from any of the
      * queue phases like buildable, blocked and waiting
      *
-     * @param task task associated with a queue
-     * @param id id of the queue
-     * @param queue map used to store the time stamp of the queue phases
-     * @param eventType type of the queue phase
-     * @param message message of the slave available in the queue
+     * @param task         task associated with a queue
+     * @param id           id of the queue
+     * @param eventType    type of the queue phase
+     * @param message      message of the slave available in the queue
      * @param inQueueSince time
      * @return
      */
-    private Float sendToSplunkOnLeft(Queue.Task task, Long id, ConcurrentHashMap queue, String eventType, String message, Long inQueueSince) {
+    private Float sendToSplunkOnLeft(Queue.Task task, Long id, String eventType, String message, Long inQueueSince) {
         if (SplunkJenkinsInstallation.get().isEventDisabled(QUEUE_INFO)) {
             return 0f;
         }
@@ -181,7 +174,7 @@ public class LoggingQueueListener extends QueueListener {
         String eventTypeDequeue;
         String durationName;
         //Customize queue time for phases Vs on left at last
-        if(eventType.equals(Constants.DEQUEUE_TAG_NAME)) {
+        if (eventType.equals(Constants.DEQUEUE_TAG_NAME)) {
             eventTypeDequeue = eventType;
             durationName = "queue_time";
             //Calculate queue time for on left from getInQueueSince available in the queueItem
@@ -190,7 +183,7 @@ public class LoggingQueueListener extends QueueListener {
             eventTypeDequeue = Constants.DEQUEUE_TAG_NAME + "_" + eventType;
             durationName = eventType + "_time";
             //Calculate duration for the phase from the stored HashMap in this object.
-            queueTimeDuration = getDurationInQueuePhase(id, queue);
+            queueTimeDuration = getDurationInQueuePhase(id);
         }
 
         Map event = getCommonEvents(eventTypeDequeue, id, name);
@@ -203,19 +196,18 @@ public class LoggingQueueListener extends QueueListener {
 
     /**
      * Calculate the time spent in a particular phase
+     *
      * @param id id of the item in queue
-     * @param queueType type of queue
      * @return time duration spent in a particular phase
      */
-    private Float  getDurationInQueuePhase(Long id, Map<Long, Long> queueType) {
-        Long startTime = queueType.get(id);
+    private Float getDurationInQueuePhase(Long id) {
+        Long startTime = queuePhase.get(id);
         Float durationInPhase;
         if (startTime == null) {
             //the queue has been garbage collected or jenkins has restarted
             durationInPhase = 0f;
         } else {
-            durationInPhase = (System.currentTimeMillis() - startTime)/1000f;
-            queueType.remove(id);
+            durationInPhase = (System.currentTimeMillis() - startTime) / 1000f;
         }
         return durationInPhase;
     }
